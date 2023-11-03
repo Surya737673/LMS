25d2caa34cea3fa1ed69993d128c7a519da252ee
Surya Pratap Yadav <99668540+Surya737673@users.noreply.github.com>
2023-11-03 09:13:08 +0530

    changes

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,8 +1,9 @@
-'use-client'
+'use client';
 import React, { FC, useState } from 'react';
 import Link from "next/link";
 import NavItems from "../utils/NavItems";
-import { ThemeProvider } from '../utils/theme-provider';
+import { ThemeSwitcher } from '../utils/ThemeSwitcher';
+import { HiOutlineMenuAlt3, HiOutlineUserCircle } from "react-icons/hi"
 
 type Props = {
     open: boolean;
@@ -10,7 +11,7 @@ type Props = {
     activeItem: number
 }
 
-const Header: FC<Props> = ({activeItem}) => {
+const Header: FC<Props> = ({ activeItem, setOpen }) => {
     const [active, setActive] = useState(false);
     const [openSidebar, setOpenSidebar] = useState(false);
 
@@ -24,6 +25,14 @@ const Header: FC<Props> = ({activeItem}) => {
         })
     }
 
+    const handleClose = (e: any) => {
+        if (e.target.id === "screen") {
+            {
+                setOpenSidebar(false);
+            }
+        }
+    }
+
     return (
         <div className='w-full relative'>
             <div className=
@@ -31,21 +40,63 @@ const Header: FC<Props> = ({activeItem}) => {
                     "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80]  border-b dark:border-[#ffffff1c] shadow-xl transition duration-500"
                     : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"
                     }`}>
-                
+
                 <div className='w-[95%] 800px:w-[92%] m-auto py-2 h-full'>
-                    <div className='w-full h-[80px] flex items-center justify-between p-3 '> 
+                    <div className='w-full h-[80px] flex items-center justify-between p-3 '>
                         <div>
                             <Link href={"/"} className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}>
                                 Elearning
                             </Link>
                         </div>
                         <div className="flex items-center">
-                            <NavItems  
-                            activeItem={activeItem} 
-                            isMobile={false} />
+                            <NavItems
+                                activeItem={activeItem}
+                                isMobile={false} />
+
+                            {/* <ThemeSwitcher /> */}
+
+                            <div className="800px:hidden">
+
+                                <HiOutlineMenuAlt3 size={25}
+                                    className="cursor-pointer dark:text-white text-black"
+                                    onClick={() => setOpenSidebar(true)} />
+
+                            </div>
+
+                            <HiOutlineUserCircle
+                                size={25}
+                                className="cursor-pointer"
+                                onClick={() => setOpen(true)}
+                            />
+
                         </div>
                     </div>
                 </div>
+
+                {/* mobile sidebar */}
+
+                {
+                    openSidebar && (
+                        <div className="fixed w-full h-screen top-0 left-0 z-[99999] dark:bg-[unset] bg-[#00000024]"
+                            onClick={handleClose}
+                            id="screen"
+                        >
+                            <div className="w-[70%] fixed z-[999999999] h-screen bg-white dark:bg-slate-900 dark:bg-opacity-90 top-0 right-0 ">
+                                <NavItems activeItem={activeItem} isMobile={true} />
+                                <HiOutlineUserCircle 
+                                        size = {25}
+                                        className="cursor-pointer ml-5 my-2 text-black dark:text-white"
+                                        onClick={() => setOpen(true)}
+                                />
+                                <br />
+                                <br />
+                                <p className='text-[16px] px-2 pl-5 text-black dark:text-white'>
+                                    Copyright @ 2023 SPY
+                                </p>
+                            </div>
+                        </div>
+                    )
+                }
 
             </div>
         </div>
--- client/app/utils/NavItems.tsx
+++ client/app/utils/NavItems.tsx
@@ -46,7 +46,13 @@ const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
             {
                 isMobile && (
                     <div className="800px:hidden mt-5">
-                        <div className="800px:hidden mt-5">
+                        <div className="w-full text-center py-6">
+                            <Link href={"/"} passHref>
+                                <span className={`text-[25px] font-Poppins font-[500] text-black dark:text-white`}>
+                                    Learning
+                                </span>
+                            </Link>
+                        </div>
                             {
                                 navItemsData && navItemsData.map((i, index) => (
                                     <Link href={`${i.url}`} key={index} passHref>
@@ -58,7 +64,6 @@ const NavItems: React.FC<Props> = ({ activeItem, isMobile }) => {
                                     </Link>
                                 ))
                             }
-                        </div>
                     </div>
                 )
             }
